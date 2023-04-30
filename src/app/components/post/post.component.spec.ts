12583e2a732c7fca4a5619a2c0709479fe719af4
jerome <zachariah11jerobert@gmail.com>
2023-04-30 09:46:09 +0530

    fix

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,13 +1,48 @@
 import { Post } from "src/app/models/Post";
 import { PostComponent } from "./post.component";
 import { first } from "rxjs";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 
 describe('Post Component', () => {
+    let fixture: ComponentFixture<PostComponent>;
+    let comp: PostComponent;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [PostComponent]
+        });
+
+        fixture = TestBed.createComponent(PostComponent);
+        comp = fixture.componentInstance;
+    });
+
+    it('should create post componnet using TestBed', () => {
+        expect(comp).toBeDefined();
+    });
+
+    it('should render the post title in the anchor element', () => {
+        const post: Post = { id: 1, body: 'body 1', title: 'any title' };
+        comp.post = post;
+        fixture.detectChanges();
+        const postElement: HTMLElement = fixture.nativeElement;
+        const a = postElement.querySelector('a');
+        expect(a?.textContent).toContain(post.title);
+    });
+
+    it('should render the post title in the anchor element using debug element', () => {
+        const post: Post = { id: 1, body: 'body 1', title: 'any title' };
+        comp.post = post;
+        fixture.detectChanges();
+        const postDebugElement = fixture.debugElement;
+        const aElement: HTMLElement = postDebugElement.query(By.css('a')).nativeElement;
+        expect(aElement.textContent).toContain(post.title);
+    });
+
+
     it('should raise an event when the delete post is clicked', () => {
-        const comp = new PostComponent();
-        const post:Post={id:1,body:'body 1',title:'any title'};
-        comp.post=post;
-        comp.delete.pipe(first()).subscribe((selectedPost)=>{
+        const post: Post = { id: 1, body: 'body 1', title: 'any title' };
+        comp.post = post;
+        comp.delete.pipe(first()).subscribe((selectedPost) => {
             expect(selectedPost).toBe(post);
         });
 
